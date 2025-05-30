0d6b2f7f66368ef21afc79064b26905758e32094
Alya Humaira <halya9533@gmail.com>
2025-05-30 16:42:13 +0800

    commit

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -46,13 +46,13 @@
                                 {{ __('Log Out') }}
                             </x-dropdown-link>
                         </form>
-                        <form method="POST" action="{{ route('logout') }}">
+                        <!-- <form method="POST" action="{{ route('logout') }}">
                             @csrf
                             <x-responsive-nav-link :href="route('logout')"
                                 onclick="event.preventDefault(); this.closest('form').submit();">
                                 {{ __('Log Out') }}
                             </x-responsive-nav-link>
-                        </form>
+                        </form> -->
                     </x-slot>
                 </x-dropdown>
             </div>
