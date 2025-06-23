727262a6ca01a60bf064c3f66a9cb6af064199eb
convestudo <halya9533@gmail.com>
2025-06-23 14:27:08 +0800

    commit

--- app/Http/Controllers/Customer/CustomerFeedbackController.php
+++ app/Http/Controllers/Customer/CustomerFeedbackController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Order;
 use App\Models\User;
-use App\Models\feedback;
+use App\Models\Feedback;
 use Carbon\Carbon;
 
 class CustomerFeedbackController extends Controller
