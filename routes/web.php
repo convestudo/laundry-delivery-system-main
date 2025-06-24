dc5383039094b4f021c3efc8ade8b560cd3fdb57
convestudo <halya9533@gmail.com>
2025-06-24 13:28:54 +0800

    commit

--- routes/web.php
+++ routes/web.php
@@ -209,7 +209,6 @@ Route::prefix('/api/service-cart')->group(function () {
     Route::post('/duplicate/{id}', [CustomerBookController::class, 'duplicate']); // duplicate specific cart item
     Route::put('/{id}', [CustomerBookController::class, 'update']);
     Route::post('/get-available-drivers', [CustomerBookController::class, 'getAvailableDrivers']);
-    Route::get('/get-available-drivers', [CustomerBookController::class, 'getAvailableDrivers']);
     Route::post('/submit-order', [CustomerBookController::class, 'submitOrder']);
     Route::post('/get-available-vouchers', [CustomerBookController::class, 'getAvailableVouchers']);
 });
